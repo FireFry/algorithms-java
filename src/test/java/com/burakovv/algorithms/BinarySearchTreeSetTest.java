8da7eef9a38ef45311e70de714a745fda611e7f1
FireFry <v.a.burakov@gmail.com>
2014-05-25 12:00:25 +0300

    Add tests for decrease and increase sequence

--- src/test/java/com/burakovv/algorithms/BinarySearchTreeSetTest.java
+++ src/test/java/com/burakovv/algorithms/BinarySearchTreeSetTest.java
@@ -10,18 +10,88 @@ import static org.junit.Assert.assertEquals;
 
 public class BinarySearchTreeSetTest {
 
-    @Test
+    @Test(timeout = 30000)
     public void testRandom() {
+        doTest(new Provider() {
+            private final Random random = new Random(31415926);
+
+            @Override
+            public int nextValue() {
+                return random.nextInt(100000);
+            }
+
+            @Override
+            public int nextAction() {
+                return random.nextInt(3);
+            }
+
+            @Override
+            public int getIterations() {
+                return 10000000;
+            }
+        });
+    }
+
+    @Test(timeout = 30000)
+    public void testInc() {
+        doTest(new Provider() {
+            private int next = 0;
+            private final Random random = new Random(31415926);
+
+            @Override
+            public int nextValue() {
+                int result = next;
+                next = (next + 1) % 100000;
+                return result;
+            }
+
+            @Override
+            public int nextAction() {
+                return random.nextInt(3);
+            }
+
+            @Override
+            public int getIterations() {
+                return 40000;
+            }
+        });
+    }
+
+    @Test(timeout = 30000)
+    public void testDec() {
+        doTest(new Provider() {
+            private int next = 99999;
+            private final Random random = new Random(31415926);
+
+            @Override
+            public int nextValue() {
+                int result = next;
+                next = next == 0 ? 99999 : next - 1;
+                return result;
+            }
+
+            @Override
+            public int nextAction() {
+                return random.nextInt(3);
+            }
+
+            @Override
+            public int getIterations() {
+                return 40000;
+            }
+        });
+    }
+
+    private void doTest(Provider provider) {
         BinarySearchTreeSet<Integer> customSet = new BinarySearchTreeSet<Integer>(Comparator.<Integer>naturalOrder());
         TreeSet<Integer> systemSet = new TreeSet<Integer>();
-        Random random = new Random(31415926);
 
         long totalSystemTime = 0;
         long totalCustomTime = 0;
 
-        for (int i = 0; i < 1000000; i++) {
-            int action = random.nextInt(3);
-            int value = random.nextInt(30);
+        for (int i = 0; i < provider.getIterations(); i++) {
+            int action = provider.nextAction();
+            int value = provider.nextValue();
             boolean expected = false;
             boolean actual = false;
             long startAt;
@@ -68,6 +138,12 @@ public class BinarySearchTreeSetTest {
 
         System.out.println("Total system time: " + totalSystemTime);
         System.out.println("Total custom time: " + totalCustomTime);
+    }
+
+    private static interface Provider {
+        int nextValue();
+        int nextAction();
+        int getIterations();
     }
 
 }
